fc24f8fb2b5f3111cfc155eda902df86ab910998
nini15 <126186307+nini15@users.noreply.github.com>
2024-01-22 15:08:46 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var model=document.getElementsByTagName("li")
+model[0].innerHTML="<b>1<b/>"
+model[1].innerHTML="<b>2</b>"
+model[2].innerHTML="<b>3</b>"
+model[3].innerHTML="<b>4</b>"
+model[4].innerHTML="<b>5</b>"
+model[5].innerHTML="<b>6</b>"
